86deb7af9b2055d9a9a8181b4885e9fe8e402f96
Arthur Stevenson <arthur.stevenson@nubank.com.br>
2025-06-20 17:22:20 -0300

    Fix: Extra copied information from a3 in a4

--- instances/series/a4.sql
+++ instances/series/a4.sql
@@ -8,9 +8,3 @@ localSsd    = '1',
 bandwidth   = '3600',
 spot        = '1'
 WHERE name LIKE 'a4-%-8g';
-
-UPDATE instances SET localSsd = '6000' WHERE name LIKE 'a3-%-8g';
-
-UPDATE instances SET bandwidth = '1000'  WHERE name LIKE 'a3-highgpu-8g';
-UPDATE instances SET bandwidth = '1800' WHERE name LIKE 'a3-megagpu-8g';
-UPDATE instances SET bandwidth = '3600' WHERE name LIKE 'a3-ultragpu-8g';
